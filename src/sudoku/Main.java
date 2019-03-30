751b4b48df6435c807f2c51c712cbac848c77c7f
Florin Barbisch <florin.barbisch@gmail.com>
2019-03-30 14:47:40 +0100

    Code optimization - Removed 76 lines of code

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,17 +1,15 @@
 package sudoku;
 
-import javax.swing.JOptionPane;
-
 public class Main {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		Sudoku s = new Sudoku();
 		long end =  System.currentTimeMillis();
-		
+
 		System.out.println("Time: " + ((double)(end - start)/1000) + " ms");
-		System.out.println(s.print());
-		
-		CSVhandler.exportSolutionCSV(s.getSudoku(), false);
-		CSVhandler.exportNormalCSV(s.getSudoku(), false);
+		System.out.println(s);
+
+		CSVhandler.exportSolutionCSV(s.getArray(), false);
+		CSVhandler.exportNormalCSV  (s.getArray(), false);
 	}
 }
--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -3,58 +3,48 @@ package sudoku;
 import java.util.Random;
 
 public class Sudoku {
-	int[][] sudoku;
-
-	public Sudoku(int[][] sudoku) {
-		this.sudoku = sudoku;
-	}
+	private int[][] sudoku;
 
 	public Sudoku() {
-		this.sudoku = generateArray();
+	  int count = 0;
+    this.sudoku = new int[9][9];
+
+    boolean b;
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        int r = Sudoku.getRandom();
+        while (b = this.allContainsN(j, i, r)) {
+          r = Sudoku.getRandom();
+          count++;
+          if (count > 50) {
+            count = 0;
+            this.fillRowWithZeros(i);
+            i--;
+            break;
+          }
+        }
+        if (!b) {
+          this.sudoku[i][j] = r;
+          count = 0;
+        }  else {
+          break;
+        }
+      }
+    }
+
 	}
 
-	public int[][] getSudoku() {
-		return sudoku;
+	public int[][] getArray() {
+		return this.sudoku;
 	}
 
-	public int[][] generateArray() {
-		int count = 0;
-		int[][] array = new int[9][9];
-		array = fillAllWithZeros();
-
-		boolean b;
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				int r = getRandom();
-				while (b = allContainsN(array, j, i, r)) {
-					r = getRandom();
-					count++;
-					if (count > 50) {
-						count = 0;
-						array = fillRowWithZeros(array, i);
-						i--;
-						break;
-					}
-				}
-				if (!b) {
-					array[i][j] = r;
-					count = 0;
-				}  else {
-					break;
-				}
-			}
-		}
-
-		return array;
-	}
-
-	public String print() {
+	@Override public String toString() {
 		StringBuilder str = new StringBuilder();
 		int countCol = 0;
 		int countRow = 0;
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				str.append("[" + sudoku[i][j] + "]");
+				str.append("[" + this.sudoku[i][j] + "]");
 				countCol++;
 				if (countCol > 2) {
 					if (j != 8)
@@ -74,110 +64,46 @@ public class Sudoku {
 		return str.toString();
 	}
 
-	private boolean allContainsN(int[][] array, int col, int row, int n) {
-		return rowContainsN(array, col, n) || colContainsN(array, row, n) || squareContainsN(array, col, row, n);
+	private boolean allContainsN(int col, int row, int n) {
+		return this.rowContainsN(col, n) || this.colContainsN(row, n) || this.squareContainsN(col, row, n);
 	}
 
-	private boolean rowContainsN(int[][] array, int col, int n) {
+	private boolean rowContainsN(int col, int n) {
 		for (int i = 0; i < 9; i++) {
-			if (array[i][col] == n)
+			if (this.sudoku[i][col] == n)
 				return true;
 		}
-
 		return false;
 	}
 
-	private boolean colContainsN(int[][] array, int row, int n) {
+	private boolean colContainsN(int row, int n) {
 		for (int i = 0; i < 9; i++) {
-			if (array[row][i] == n)
+			if (this.sudoku[row][i] == n)
 				return true;
 		}
-
 		return false;
 	}
 
-	private boolean squareContainsN(int[][] array, int col, int row, int n) {
-		int minRow = 0, maxRow = 0, minCol = 0, maxCol = 0;
-
-		switch (col) {
-		case 0:
-		case 1:
-		case 2:
-			minCol = 0;
-			maxCol = 2;
-			break;
-		case 3:
-		case 4:
-		case 5:
-			minCol = 3;
-			maxCol = 5;
-			break;
-		case 6:
-		case 7:
-		case 8:
-			minCol = 6;
-			maxCol = 8;
-			break;
-		default:
-			System.exit(0);
-		}
-
-		switch (row) {
-		case 0:
-		case 1:
-		case 2:
-			minRow = 0;
-			maxRow = 2;
-			break;
-		case 3:
-		case 4:
-		case 5:
-			minRow = 3;
-			maxRow = 5;
-			break;
-		case 6:
-		case 7:
-		case 8:
-			minRow = 6;
-			maxRow = 8;
-			break;
-		default:
-			System.exit(0);
-		}
-
-		for (int i = minRow; i <= maxRow; i++) {
-			for (int j = minCol; j <= maxCol; j++) {
-				if (array[i][j] == n)
+	private boolean squareContainsN(int col, int row, int n) {
+		for (int i = 0; i <= 2; i++) {
+			for (int j = 0; j <= 2; j++) {
+				if (this.sudoku[row / 3 * 3 + i][col / 3 * 3 + j] == n)
 					return true;
 			}
 		}
-
 		return false;
 	}
 
-	private int[][] fillAllWithZeros() {
-		int array[][] = new int[9][9];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				array[i][j] = 0;
-			}
-		}
-		return array;
+	private void fillRowWithZeros(int row) {
+		this.sudoku[row] = new int[9];
 	}
 
-	private int[][] fillRowWithZeros(int[][] array, int row) {
-		for (int i = 0; i < 9; i++) {
-			array[row][i] = 0;
-		}
-		return array;
-	}
-
-	private int getRandom() {
-		int r = 0;
-		while (r == 0) {
-			r = new Random().nextInt(10);
-		}
-		return r;
+	/**
+	 * Returns a random int between 1 and 9.
+	 * @return
+	 */
+	private static int getRandom() {
+		return new Random().nextInt(9) + 1;
 	}
 
 }
